68bb2ea4688860dd176bb6d321849816983df5b1
adnaan512 <hassnain.bscs21seecs@seecs.edu.pk>
2025-08-11 16:45:27 +0500

    Update chatbot logic to fix config handling in invoke

--- basic_chatbot.py
+++ basic_chatbot.py
@@ -2,6 +2,7 @@ from langgraph.graph import StateGraph,START, END
 from typing import TypedDict,Annotated
 from langchain_core.messages import BaseMessage ,HumanMessage
 from langchain_groq import ChatGroq
+from langgraph.checkpoint.memory import MemorySaver
 from dotenv import load_dotenv
 import os
 load_dotenv()
@@ -14,13 +15,17 @@ def chat_node(state:chatsate):
     messages=state["messages"]
     response=llm.invoke(input=messages)
     return {"messages": [response]}
+checkpointer=MemorySaver()
 graph=StateGraph(chatsate)
 graph.add_node("chat_node", chat_node)
 graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
-chatbot=graph.compile()
-intialstate={
-    "messages": [HumanMessage(content="Hello, how are you?")]
-}
-res=chatbot.invoke(intialstate)
-print(res)
+chatbot=graph.compile(checkpointer=checkpointer)
+thread_id="1"
+while True:
+    user_input = input("type your message: ")
+    if user_input.strip().lower() in ["exit", "quit", "bye"]:
+        break
+    config={"configurable" :{"thread_id": thread_id}}
+    response = chatbot.invoke({"messages": [HumanMessage(content=user_input)]}, config=config)
+    print(response["messages"][-1].content)  # Print the response from the chatbot
